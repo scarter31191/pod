8fdebbaf17a8525ea5742afc263f264268866b54
scarter31191 <scarter31191@gmaile.com>
2021-01-27 22:03:58 -0500

    Created Topic model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   root 'sessions#welcome'
 
   get '/sign-up' => 'users#new'
@@ -8,5 +9,6 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
+  resources :topics
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_034258) do
+ActiveRecord::Schema.define(version: 2021_01_28_022345) do
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
