ca1e64b7972c437f5537f514ca7b04d4542d46b3
scarter31191 <scarter31191@gmaile.com>
2021-01-27 23:33:54 -0500

    Built topic new/show action

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -9,6 +9,23 @@ class TopicsController < ApplicationController
     end
 
     def create
-        @topic
+        @topic = Topic.new(topic_params)
+        if @topic.save
+            redirect_to topic_path(@topic)
+        else
+            render :new
+        end
     end
+
+    def show
+        @topic = Topic.find(params[:id])
+    end
+
+
+
+    private
+
+    def topic_params
+        params.require(:topic).permit(:title, :body)
+      end
 end
